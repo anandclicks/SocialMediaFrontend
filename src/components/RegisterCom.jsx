09a7b2a770022d9c72abedec3efd4ab46b9dc627
anandclicks <146056018+anandclicks@users.noreply.github.com>
2025-03-07 00:02:09 +0530

    login and signup form inputs store

--- src/components/RegisterCom.jsx
+++ src/components/RegisterCom.jsx
@@ -1,18 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from "react-router-dom"
 import { OtpContet } from '../../contentProvider/Otp.content'
 
 const RegisterCom = () => {
     const {setwantOtp} = useContext(OtpContet)
+    // handling input and storing data in local
+    const [formData, setformData] = useState({
+      name : '',
+      email : '',
+      username : '',
+      number : '',
+      password : ''
+    })
+    const handleInput = (event)=> {
+      setformData(prev=>({...prev, [event.target.name] : event.target.value}))
+    }
+
   return (
     <form className=" w-[55%]">
     <h2 className="text-4xl mb-1 signUpTitle">Sign up</h2>
     <p className=" leading-6 mb-4">Create Your Account in Just a Few Steps!</p>
-    <input placeholder="Enter Name" name="name" required className="text-sm h-[40px] w-[100%] border-[1px] border-stone-400 outline-0 p-3 rounded-full mb-4" type="text" />
-    <input placeholder="Enter Email" name="email" required className="text-sm h-[40px] w-[100%] border-[1px] border-stone-400 outline-0 p-3 rounded-full mb-4" type="email" />
-    <input placeholder="Enter Username" name="username" required className="text-sm h-[40px] w-[100%] border-[1px] border-stone-400 outline-0 p-3 rounded-full mb-4" type="text" />
-    <input placeholder="Enter Number" name="number" className="text-sm h-[40px] w-[100%] border-[1px] border-stone-400 outline-0 p-3 rounded-full mb-4" type="number" />
-    <input placeholder="Enter Password" name="password" required className="text-sm h-[40px] w-[100%] border-[1px] border-stone-400 outline-0 p-3 rounded-full mb-4" type="text" />
+    <input onChange={(evt)=> handleInput(evt)} value={formData.name} placeholder="Enter Name" name="name" required className="text-sm h-[40px] w-[100%] border-[1px] border-stone-400 outline-0 p-3 rounded-full mb-4" type="text" />
+    <input onChange={(evt)=> handleInput(evt)} value={formData.email} placeholder="Enter Email" name="email" required className="text-sm h-[40px] w-[100%] border-[1px] border-stone-400 outline-0 p-3 rounded-full mb-4" type="email" />
+    <input onChange={(evt)=> handleInput(evt)} value={formData.username} placeholder="Enter Username" name="username" required className="text-sm h-[40px] w-[100%] border-[1px] border-stone-400 outline-0 p-3 rounded-full mb-4" type="text" />
+    <input onChange={(evt)=> handleInput(evt)} value={formData.number} placeholder="Enter Number" name="number" className="text-sm h-[40px] w-[100%] border-[1px] border-stone-400 outline-0 p-3 rounded-full mb-4" type="number" />
+    <input onChange={(evt)=> handleInput(evt)} value={formData.password} placeholder="Enter Password" name="password" required className="text-sm h-[40px] w-[100%] border-[1px] border-stone-400 outline-0 p-3 rounded-full mb-4" type="text" />
 
     {/* submit btn  */}
     <div className="flex justify-between items-center">
